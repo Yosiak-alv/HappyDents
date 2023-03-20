95150a1bf6e9359f0b6ffe90a3d025e07419c766
Duque206 <rafaelduque124@gmail.com>
2023-03-20 15:17:00 -0600

    Se actualizó el apartado del sistema del paciente
    Corrección de semántica

--- resources/js/Pages/Clinic/Patients/Patient_Systems/Index.vue
+++ resources/js/Pages/Clinic/Patients/Patient_Systems/Index.vue
@@ -16,21 +16,21 @@
 <template>
     <div v-if="props.patient_systems.length !== 0 ">
         <header>
-            <h2 class="h2">Sistema Inmunologico</h2>
+            <h2 class="h2">Sistema Afectado</h2>
 
             <p class="p">
-                Informacion sobre el sistema inmunologico del paciente.
+                Informacion sobre el sistema biológico afectado del paciente.
             </p>
         </header>
         <div class="flex justify-between mt-6">
-            <Link :href="route('pacienteSistema.edit',props.patient_id)" 
-                as="button" class="btn btn-primary">Editar Sistema Inmunologico</Link>
+            <Link :href="route('pacienteSistema.edit',props.patient_id)"
+                as="button" class="btn btn-primary">Editar Sistema Afectado</Link>
         </div>
         <table class="table mt-3 text-center">
             <thead>
                 <tr>
                     <th scope="col">Codigo</th>
-                    <th scope="col">Sistema Inmunologico</th>
+                    <th scope="col">Sistema Afectado</th>
                 </tr>
             </thead>
             <tbody class="table-group-divider ">
@@ -43,7 +43,7 @@
     </div>
     <div v-else>
         <p class="mt-1 text-sm text-gray-600 p-5 text-center">
-            El paciente no posee Sistema Inmunologico.
+            El paciente no posee condición en su sistema.
             <div class="py-4">
                 <Link :href="route('pacienteSistema.create',props.patient_id)"  method="get" as="button" class="btn btn-secondary">Actualizar Informacion.</Link>
             </div>
