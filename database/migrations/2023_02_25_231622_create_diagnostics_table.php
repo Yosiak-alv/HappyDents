5cbd5a777fbce7c5d012bd1848d07052658b39c2
Yosiak-alv <Josias_romero60@outlook.com>
2023-04-16 00:16:04 -0600

    Database Softdeletes, Cascadeon Delete etc

--- database/migrations/2023_02_25_231622_create_diagnostics_table.php
+++ database/migrations/2023_02_25_231622_create_diagnostics_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('diagnostics', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\clinic\Patient::class)->nullable(false)->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignIdFor(\App\Models\clinic\Patient::class)->nullable(false)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->text('observation');
             $table->text('diagnostic');
             $table->date('date')->nullable()->default(date("Y-m-d"));
--- database/migrations/2023_02_25_234229_create_family_backgrounds_table.php
+++ database/migrations/2023_02_25_234229_create_family_backgrounds_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('family_backgrounds', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\clinic\Patient::class)->nullable(false)->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignIdFor(\App\Models\clinic\Patient::class)->nullable(false)->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->foreignIdFor(\App\Models\clinic\System::class)->nullable(false)->constrained()->cascadeOnUpdate()->restrictOnDelete();
             $table->string('relationship');
             $table->string('condition');
