6f1c2412504a998e44d4efe85fd5bea6f58c98f4
Yosiak-alv <Josias_romero60@outlook.com>
2023-04-16 00:26:43 -0600

    Config Policices

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,14 +4,20 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use App\Models\clinic\Detention;
+use App\Models\clinic\Patient;
 use App\Models\clinic\System;
+use App\Models\clinic\Treatment;
 use App\Models\User;
 use App\Models\clinic\Diagnostic;
 use App\Policies\DiagnosticPolicy;
 use App\Policies\OdontogramPolicy;
+use App\Policies\PatientPolicy;
 use App\Policies\SystemPolicy;
+use App\Policies\TreatmentPolicy;
 use App\Policies\UserPolciy;
+use App\Policies\VisitPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use League\Flysystem\UnixVisibility\VisibilityConverter;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,6 +30,9 @@ class AuthServiceProvider extends ServiceProvider
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         //'App\Models\User' => 'App\Policies\UserPolicy',
         User::class => UserPolciy::class, //si no la agrago no funciona
+        Visit::class => VisitPolicy::class,
+        Patient::class => PatientPolicy::class,
+        //Treatment::class => TreatmentPolicy::class,
         //patients sin esto funciona ?
         //Diagnostic::class => DiagnosticPolicy::class , //no lo he probado xd funciona sin agregarlo
         //familybackground funciona si agregarlo aqui ?
