9cff88afc574680325db0513301a7cc0595fd6b7
Luis Bonilla <124472850+bocchiko@users.noreply.github.com>
2023-05-06 20:32:28 -0600

    Modificación a Seeders
    Se agregan los datos reales utilizados por la clínica a los seeders que se encargan de poblar las tablas de: odontograma, tratamientos y sistemas.

--- database/seeders/TreatmentSeeder.php
+++ database/seeders/TreatmentSeeder.php
@@ -14,6 +14,59 @@ class TreatmentSeeder extends Seeder
      */
     public function run()
     {
-        Treatment::factory(15)->create();
+        
+        $Treatments = [
+            'Limpieza Dental',
+            'Obturaciones',
+            'Exodoncia Simple',
+            'Exodoncia Complicada',
+            'Cirugia Simple',
+            'Cirugia Complicada',
+            'Endoncia Monoradicular',
+            'Endodoncia Multiradicular',
+            'Reconstruccion Poste de Fibra de Vidrio',
+            'Reconstruccion Forma Dentina',
+            'Recosntruccion Incrustacion',
+            'Corona Metal Porcelana',
+            'Corona Porcelana',
+            'Corona Ceromero',
+            'Valplas Unilateral',
+            'Valplas Bilateral',
+            'Removible Convencional',
+            'Protesis Completa',
+            'Cirugia Periodontal',
+            'Implantes',
+            'Ortodoncia'
+        ];
+
+        $Prices = [
+            25.00,
+            30.00,
+            25.00,
+            50.00,
+            50.00,
+            100.00,
+            125.00,
+            175.00,
+            70.00,
+            90.00,
+            200.00,
+            100.00,
+            200.00,
+            225.00,
+            300.00,
+            600.00,
+            300.00,
+            125.00,
+            200.00,
+            1500.00,
+            1500.00
+        ];
+
+        Treatment::factory(count($Treatments))->sequence(fn($sequence) => [
+            'name' => $Treatments[$sequence->index],
+            'price' => $Prices[$sequence->index]
+        ])->create();
+
     }
 }
