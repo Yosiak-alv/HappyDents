aeb6a927ea86113308e99690f1146724d4c73fb9
Cotto213 <alfonsocotto5b12@gmail.com>
2023-05-07 18:05:02 -0600

    Se actualizó el odontograma

--- resources/js/Pages/Clinic/Patients/Patient_Odontogram/CreateEditPatientOdontogram.vue
+++ resources/js/Pages/Clinic/Patients/Patient_Odontogram/CreateEditPatientOdontogram.vue
@@ -20,12 +20,12 @@
         selected_odontogram_conditions:{
             type:Object,
         },
-        
+
     });
     const form = useForm({
         patient_id: props.patient.id,
         detention_id: props.selected_patient_odontogram_detentions ?? [],
-        condition: props.selected_odontogram_conditions ?? [] 
+        condition: props.selected_odontogram_conditions ?? []
     });
     const store = () =>{
         form.post(route('pacienteOdontograma.store',form.patient_id));
@@ -53,28 +53,36 @@
         });
     };
 </script>
+
 <template>
-    <HappyDentsLayout>
-        <Head title="Paciente-Odontograma"/>
-        <div class="container position-absolute top-50 start-50 translate-middle">
-            <div class="col-md-12 text-center mb-4">
-                <p class="h4">Paciente:</p>
+    <p class="h4">Paciente:</p>
                 <p class="p">{{patient.name}}</p>
-            </div>
-            <div class="col-md-10 offset-md-1 p-5 bg-light border rounded-3">
-                <PatientOdontogramForm :updating="(props.selected_patient_odontogram_detentions === undefined ? false:true)" 
-                :form="form" :detentions="props.detentions" @submit.prevent="(props.selected_patient_odontogram_detentions === undefined ? store() : update())"/>         
-                <div class="col-md-12 text-right">
-                    <div v-if="props.selected_patient_odontogram_detentions != null && (usePage().props.auth.user.role.type == 'administrador' || usePage().props.auth.user.role.type == 'doctor')">
-                        <button @click="confirmOdontogramDeletion" 
-                            class="btn btn-danger mt-3">Eliminar el Odontograma
-                        </button>
-                    </div>
-                    <div v-else></div>
-                </div>
-            </div>  
+                <HappyDentsLayout>
+  <Head title="Paciente-Odontograma"/>
+  <div class="container-fluid overflow-auto h-100">
+    <div class="row">
+      <div class="col-md-16 text-center mb-4">
+        <p class="h4">Paciente:</p>
+        <p class="p">{{patient.name}}</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-15 offset-md-1 p-1 bg-light border rounded-3">
+        <PatientOdontogramForm :updating="(props.selected_patient_odontogram_detentions === undefined ? false:true)"
+          :form="form" :detentions="props.detentions" @submit.prevent="(props.selected_patient_odontogram_detentions === undefined ? store() : update())"/>
+        <div class="col-md-12 text-right">
+          <div v-if="props.selected_patient_odontogram_detentions != null && (usePage().props.auth.user.role.type == 'administrador' || usePage().props.auth.user.role.type == 'doctor')">
+            <button @click="confirmOdontogramDeletion" class="btn btn-danger mt-3">Eliminar el Odontograma</button>
+          </div>
+          <div v-else></div>
         </div>
-    </HappyDentsLayout>
+      </div>
+    </div>
+  </div>
+</HappyDentsLayout>
+
+
+
 
     <Modal :show="confirmingOdontogramDeletion" @close="closeModal">
         <div class="container">
@@ -85,8 +93,8 @@
                     </h4>
                 </div>
                 <div class="col-12">
-                    <p class="p p-4"> 
-                        Si lo Elimina, el registro se borrara PERMANETEMENTE, sin posibilidad de 
+                    <p class="p p-4">
+                        Si lo Elimina, el registro se borrara PERMANETEMENTE, sin posibilidad de
                         recuperacion de la informacion del registro, desea continuar ?
                     </p>
                 </div>
